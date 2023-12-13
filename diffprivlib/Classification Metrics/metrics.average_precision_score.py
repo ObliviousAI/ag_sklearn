f0df2396519584e61b93e4e1ff7d94c1e4477c44
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 01:17:06 +0530

    Update metrics.average_precision_score.py

--- diffprivlib/Classification Metrics/metrics.average_precision_score.py
+++ diffprivlib/Classification Metrics/metrics.average_precision_score.py
@@ -3,7 +3,7 @@ import numpy as np
 from diffprivlib.tools.utils import mean
 from diffprivlib.accountant import BudgetAccountant
 
-def average_precision_score(y_true, y_score, epsilon=1.0,bounds=None, random_state=None):
+def average_precision_score(y_true, y_score, epsilon=1.0, bounds=None, random_state=None):
     # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_score = np.asarray(y_score)
@@ -12,7 +12,7 @@ def average_precision_score(y_true, y_score, epsilon=1.0,bounds=None, random_sta
     avg_precision = metrics.average_precision_score(y_true, y_score)
 
     # Add differential privacy to the average precision calculation
-    dp_mean = mean(y_score, bounds=bounds, epsilon=epsilon, random_state=random_state, accountant=BudgetAccountant())
-    dp_avg_precision = dp_mean  # You may adjust this according to your DP calculation
+    accountant = BudgetAccountant()
+    dp_mean = mean(y_score, bounds=bounds, epsilon=epsilon, random_state=random_state, accountant=accountant)
 
-    return dp_avg_precision
+    return avg_precision + dp_mean
