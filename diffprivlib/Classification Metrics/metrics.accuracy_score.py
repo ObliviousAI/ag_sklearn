295716a8e026db3c8ec30d2b29692e3aa93790c6
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 00:56:49 +0530

    Update metrics.accuracy_score.py

--- diffprivlib/Classification Metrics/metrics.accuracy_score.py
+++ diffprivlib/Classification Metrics/metrics.accuracy_score.py
@@ -1,9 +1,10 @@
 from sklearn import metrics
 import numpy as np
-
+from diffprivlib.tools.utils import mean
+from diffprivlib.accountant import BudgetAccountant
 # Define differentially private mean functions here
 
-def accuracy_score(y_true, y_pred, epsilon=1.0, bounds=None):
+def accuracy_score(y_true, y_pred, epsilon=1.0, bounds=None, random_state=None):
     # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_pred = np.asarray(y_pred)
@@ -12,6 +13,6 @@ def accuracy_score(y_true, y_pred, epsilon=1.0, bounds=None):
     accuracy = metrics.accuracy_score(y_true, y_pred)
 
     # Add differential privacy to the accuracy calculation
-    dp_accuracy = mean(np.equal(y_true, y_pred), epsilon=epsilon, bounds=bounds)
+    dp_accuracy = mean(np.equal(y_true, y_pred), bounds=bounds, epsilon=epsilon, random_state=random_state, accountant=BudgetAccountant())
 
     return dp_accuracy
