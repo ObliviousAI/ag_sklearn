6d5eb1063576b20cf8a138b78d5f2cd37109089c
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 04:39:22 +0530

    Update and rename metrics.average_precision_score.py to average_precision_score.py

--- diffprivlib/Classification_Metrics/average_precision_score.py
+++ diffprivlib/Classification_Metrics/average_precision_score.py
@@ -4,14 +4,11 @@ from diffprivlib.tools.utils import mean
 from diffprivlib.accountant import BudgetAccountant
 
 def average_precision_score(y_true, y_score, epsilon=1.0, bounds=None, random_state=None):
-    # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_score = np.asarray(y_score)
-
-    # Calculate average precision using the regular function
+    
     avg_precision = metrics.average_precision_score(y_true, y_score)
 
-    # Add differential privacy to the average precision calculation
     accountant = BudgetAccountant()
     dp_mean = mean(y_score, bounds=bounds, epsilon=epsilon, random_state=random_state, accountant=accountant)
 
