133d0459674734b3aaee9abd9c6c4eea4a52dcbc
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 01:09:32 +0530

    Update metrics.f1_score.py

--- diffprivlib/Classification Metrics/metrics.f1_score.py
+++ diffprivlib/Classification Metrics/metrics.f1_score.py
@@ -1,8 +1,9 @@
 from sklearn import metrics
 import numpy as np
 from diffprivlib.tools.utils import mean
+from diffprivlib.accountant import BudgetAccountant
 
-def f1_score(y_true, y_pred, epsilon=1.0):
+def f1_score(y_true, y_pred, epsilon=1.0, bounds=None, random_state=None):
     # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_pred = np.asarray(y_pred)
@@ -10,8 +11,15 @@ def f1_score(y_true, y_pred, epsilon=1.0):
     # Calculate F1 score using the regular function
     f1 = metrics.f1_score(y_true, y_pred)
 
-    # Add differential privacy to the F1 score calculation
-    dp_mean = mean(y_pred, epsilon=epsilon)
-    dp_f1 = f1  # You may adjust this according to your DP calculation
+    # Calculate the differentially private mean of y_pred
+    dp_mean = mean(y_pred, bounds=bounds, epsilon=epsilon, random_state=random_state, accountant=BudgetAccountant())
+
+    # Introduce Laplace noise for differential privacy
+    sensitivity = 1.0  # Placeholder sensitivity, adjust based on your data
+    scale = sensitivity / epsilon
+    laplace_noise = np.random.laplace(scale=scale, size=1)
+
+    # Adjust the F1 score using dp_mean and Laplace noise
+    dp_f1 = f1 + dp_mean + laplace_noise  # You can adjust the combination based on your requirements
 
     return dp_f1
