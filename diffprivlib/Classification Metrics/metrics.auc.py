5b4faf5622b0e003d358a225ea3cb8475c91e1a7
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 04:04:59 +0530

    Update metrics.auc.py

--- diffprivlib/Classification Metrics/metrics.auc.py
+++ diffprivlib/Classification Metrics/metrics.auc.py
@@ -5,18 +5,10 @@ from diffprivlib.accountant import BudgetAccountant
 
 
 def auc(y_true, y_score, epsilon=1.0, bins=10, range=None):
-    # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_score = np.asarray(y_score)
 
-    # Calculate AUC using the regular auc function
-    auc = metrics.auc(y_true, y_score)
-
-    # Add differential privacy to the AUC calculation
     dp_hist, bin_edges = histogram(y_score, epsilon=epsilon, bins=bins, range=range, accountant=BudgetAccountant())
     dp_auc = dp_hist.sum() / len(y_true)
 
     return dp_auc
-
-# Example usage:
-# auc, dp_auc = dp_auc(y_true, y_score, epsilon=1.0, bins=10, range=(0, 1))
