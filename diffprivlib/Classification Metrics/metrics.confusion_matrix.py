a5ddaa5bb2bceab42ff319a81b5e53e1169e5f38
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 00:44:45 +0530

    Update metrics.confusion_matrix.py

--- diffprivlib/Classification Metrics/metrics.confusion_matrix.py
+++ diffprivlib/Classification Metrics/metrics.confusion_matrix.py
@@ -2,7 +2,7 @@ from sklearn import metrics
 import numpy as np
 from diffprivlib.tools.utils import mean
 
-def dp_confusion_matrix(y_true, y_pred, epsilon=1.0):
+def confusion_matrix(y_true, y_pred, epsilon=1.0):
     # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_pred = np.asarray(y_pred)
@@ -14,4 +14,4 @@ def dp_confusion_matrix(y_true, y_pred, epsilon=1.0):
     dp_mean = mean(y_pred, epsilon=epsilon)
     dp_conf_matrix = conf_matrix  # You may adjust this according to your DP calculation
 
-    return conf_matrix, dp_conf_matrix
+    return dp_conf_matrix
