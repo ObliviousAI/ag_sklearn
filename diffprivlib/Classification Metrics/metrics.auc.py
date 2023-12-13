65b5f1865676700077d17f212a7c41fc54004fe0
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 01:13:33 +0530

    Update metrics.auc.py

--- diffprivlib/Classification Metrics/metrics.auc.py
+++ diffprivlib/Classification Metrics/metrics.auc.py
@@ -1,6 +1,8 @@
 from sklearn import metrics
 import numpy as np
 from diffprivlib.tools.histograms import histogram
+from diffprivlib.accountant import BudgetAccountant
+
 
 def auc(y_true, y_score, epsilon=1.0, bins=10, range=None):
     # Convert labels to numpy arrays if they're not already
@@ -11,7 +13,7 @@ def auc(y_true, y_score, epsilon=1.0, bins=10, range=None):
     auc = metrics.auc(y_true, y_score)
 
     # Add differential privacy to the AUC calculation
-    dp_hist, bin_edges = histogram(y_score, epsilon=epsilon, bins=bins, range=range)
+    dp_hist, bin_edges = histogram(y_score, epsilon=epsilon, bins=bins, range=range, accountant=BudgetAccountant())
     dp_auc = dp_hist.sum() / len(y_true)
 
     return dp_auc
