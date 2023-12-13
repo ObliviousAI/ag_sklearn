822f4675d4c09d51961afa96620aaf605a709eba
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-14 00:42:41 +0530

    Update metrics.auc.py

--- diffprivlib/Classification Metrics/metrics.auc.py
+++ diffprivlib/Classification Metrics/metrics.auc.py
@@ -2,7 +2,7 @@ from sklearn import metrics
 import numpy as np
 from diffprivlib.tools.histograms import histogram
 
-def dp_auc(y_true, y_score, epsilon=1.0, bins=10, range=None):
+def auc(y_true, y_score, epsilon=1.0, bins=10, range=None):
     # Convert labels to numpy arrays if they're not already
     y_true = np.asarray(y_true)
     y_score = np.asarray(y_score)
@@ -14,7 +14,7 @@ def dp_auc(y_true, y_score, epsilon=1.0, bins=10, range=None):
     dp_hist, bin_edges = histogram(y_score, epsilon=epsilon, bins=bins, range=range)
     dp_auc = dp_hist.sum() / len(y_true)
 
-    return auc, dp_auc
+    return dp_auc
 
 # Example usage:
 # auc, dp_auc = dp_auc(y_true, y_score, epsilon=1.0, bins=10, range=(0, 1))
