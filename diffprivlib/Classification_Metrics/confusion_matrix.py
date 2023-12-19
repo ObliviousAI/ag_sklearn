049e20ddd35a98dfaa7b4bc83feecefcb6a74c87
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-20 00:55:11 +0530

    Update confusion_matrix.py

--- diffprivlib/Classification_Metrics/confusion_matrix.py
+++ diffprivlib/Classification_Metrics/confusion_matrix.py
@@ -1,5 +1,9 @@
 from sklearn import metrics
 import numpy as np
+from opendp.mod import enable_features
+from opendp.measurements import  make_base_laplace
+from opendp.domains import atom_domain
+from opendp.metrics import absolute_distance
 
 def confusion_matrix(y_true, y_pred, epsilon=1.0,bounds=None, random_state=None):
     y_true = np.asarray(y_true)
@@ -13,9 +17,15 @@ def confusion_matrix(y_true, y_pred, epsilon=1.0,bounds=None, random_state=None)
     
     for i in range(dp_conf_matrix.shape[0]):
         for j in range(dp_conf_matrix.shape[1]):
-            scale = sensitivity / epsilon
-            laplace_noise = np.random.laplace(scale=scale, size=1)
-            dp_conf_matrix[i, j] += laplace_noise
+            # Enable advanced features for OpenDP
+            enable_features("contrib")
+
+            laplace_scale = sensitivity / epsilon
+
+            input_space = atom_domain(T=float), absolute_distance(T=float)
+            base_lap = make_base_laplace(*input_space, scale=laplace_scale)    
+
+            dp_conf_matrix[i][j] = base_lap( dp_conf_matrix[i, j])
             
     return dp_conf_matrix
     
