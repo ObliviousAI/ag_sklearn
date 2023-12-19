dae18276b45661d5dc397d2811a8a0fde426fbe0
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-12-20 01:23:46 +0530

    Update average_precision_score.py

--- diffprivlib/Classification_Metrics/average_precision_score.py
+++ diffprivlib/Classification_Metrics/average_precision_score.py
@@ -1,17 +1,29 @@
+from sklearn.metrics import average_precision_score
+from opendp.mod import enable_features
+from opendp.measurements import  make_base_laplace
+from opendp.domains import atom_domain
+from opendp.metrics import absolute_distance
 
-from diffprivlib.Classification_Metrics.confusion_matrix import confusion_matrix
+def average_precision(y_true, y_pred, epsilon=1.0):
 
-def average_precision(y_true, y_pred, epsilon=1.0, bounds=None, random_state=None):
+    output = average_precision_score(y_true, y_pred)
 
-    conf_matrix=confusion_matrix(y_true, y_pred, epsilon=epsilon,bounds=bounds, random_state=random_state)
+    # Enable advanced features for OpenDP
+    enable_features("contrib")
 
-    tp = conf_matrix[1, 1]
-    fp = conf_matrix[0, 1]
-    fn = conf_matrix[1, 0]
+    sensitivity=1
 
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
+    laplace_scale = sensitivity / epsilon
+
+    input_space = atom_domain(T=float), absolute_distance(T=float)
+    base_lap = make_base_laplace(*input_space, scale=laplace_scale)    
+
+    output = base_lap(output)
+
+    return output
+
     
-    avg_precision = precision * recall if (precision + recall) > 0 else 0
 
-    return avg_precision
+
+
+    
